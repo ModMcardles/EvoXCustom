106bfe609434a5ce824c91b4b5bf79887f51206a
Modestas Banys <67320977+ModMcardles@users.noreply.github.com>
2020-07-13 10:34:01 +0100

    Update 1222_own_java.js

--- 1222_own_java.js
+++ 1222_own_java.js
@@ -1,7 +1,7 @@
 // SALE POPUP
 // Time variables
-var c_startDateTime = new Date("06-26-2020 00:00:01");
-var c_endDateTime = new Date("06-29-2020 23:59:59");
+var c_startDateTime = new Date("07-13-2020 00:00:01");
+var c_endDateTime = new Date("07-26-2020 23:59:59");
 var c_todayTime = new Date();
 
 // Popup activation
